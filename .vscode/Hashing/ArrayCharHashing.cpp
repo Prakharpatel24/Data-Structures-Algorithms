2ae9a827ba59fd13b3bf4d0e56848224674ad47c
Prakharpatel24 <127961789+Prakharpatel24@users.noreply.github.com>
2024-01-06 19:13:10 +0530

    Create ArrayCharHashing.cpp
    Char Hashing for an array containing mixed characters.

---
+++ .vscode/Hashing/ArrayCharHashing.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+
+    //pre computation
+    int hash[256]={0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]]++;
+    }
+
+    //queries
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        //fetch
+        cout<<hash[c]<<" ";
+    }
+}
