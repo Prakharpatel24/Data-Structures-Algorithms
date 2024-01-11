de1e1ff77763284f15d240ba553ac6ae6469c284
Prakharpatel24 <127961789+Prakharpatel24@users.noreply.github.com>
2024-01-11 07:55:06 +0530

    Update SelectionSort.cpp

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 void selectionSort(int arr[], int n){
-    for(int i=0; i<=n-2; i++){
-        int min=i;
-        for(int j=i; j<n; j++){
-            if(arr[j]<arr[min]) min=j;
+    for(int i=0; i<n-1; i++){
+        int mini=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[mini]) mini=j;
         }
-        int temp= arr[min];
-        arr[min]=arr[i];
-        arr[i]=temp;
+        int temp= arr[i];
+        arr[i]=arr[mini];
+        arr[mini]=temp;
     }
 }
 
